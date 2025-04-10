2a36bd0be0efaaefebc4057b5acbaed8dbd789bf
moore <moore@tradev-group.com>
2025-04-10 13:50:29 +0800

    add peru colombia

---
+++ v2/peru/ConstantV2.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * param
+ */
+const CURRENCY_IDR = "IDR";
+const CURRENCY_THB = "THB";
+const CURRENCY_BRL = "BRL";
+const CURRENCY_INR = "INR";
+const CURRENCY_MXN = "MXN";
+const CURRENCY_PEN = "PEN";
+const CURRENCY_COL = "COL";
+const CURRENCY_CLP = "CLP";
+
+const INDONESIA_CODE = 10;
+const THAILAND_CODE = 11;
+const INDIA_CODE = 12;
+const BRAZIL_CODE = 13;
+
+const MEXICO_CODE = 14;
+const PERU_CODE = 15;
+const COLOMBIA_CODE = 16;
+const CHILE_CODE = 17;
+
+
+const TRADE_TYPE_PAY_IN = 1;
+const TRADE_TYPE_PAY_OUT = 2;
+/**
+ * DateTime
+ */
+const F_0 = "yyyy-MM-dd'T'HH:mm:ssXXX";
+/**
+ * Payin_API.
+ * From docs API
+ */
+const PAY_IN_API = "/v2.0/transaction/pay-in";
+
+/**
+ * Payout_API.
+ * From docs API
+ */
+const PAY_OUT_API = "/v2.0/disbursement/cash-out";
+
+const CONTENT_TYPE = "application/json";
+
+const BASE_URL = 'https://gateway.smilepayz.com';
+const BASE_URL_SANDBOX = 'https://sandbox-gateway.smilepayz.com';
+
+
+
---
+++ v2/peru/InquiryOrderStatusDemo.php
@@ -0,0 +1,96 @@
+<?php
+require 'Signature.php';
+include "ConstantV2.php";
+
+class InquiryOrderStatusDemo
+{
+
+    public function doInquiryOrderStatus($env, $merchant_id, $merchant_secret, $private_key, $trade_type, $trade_no, $order_no)
+    {
+        //production
+        $requestPath = "";
+        if ($env == "production") {
+            $requestPath = BASE_URL . "/v2.0/inquiry-status";
+        }
+        if ($env == "sandbox") {
+            $requestPath = BASE_URL_SANDBOX . "/v2.0/inquiry-status";
+        }
+
+        //get time
+        $currentTime = new DateTime('now', new DateTimeZone('UTC'));
+        $currentTime->setTimezone(new DateTimeZone('Asia/Bangkok'));
+        $timestamp = $currentTime->format('Y-m-d\TH:i:sP');
+
+        echo "timestamp=" . $timestamp . PHP_EOL;
+
+        //generate parameter $inquiryOrderStatusReq
+        $inquiryOrderStatusReq = array(
+            'tradeType' => $trade_type,
+            'tradeNo' => $trade_no,
+            'orderNo' => $order_no
+        );
+        $signUtils = new Signature();
+
+        //json
+        $jsonString = $signUtils->minify($inquiryOrderStatusReq);
+        echo "request parameters jsonString=" . $jsonString . PHP_EOL;
+
+        //build
+        $stringToSign = $timestamp . "|" . $merchant_secret . "|" . $jsonString;
+        echo "before sign stringToSign=" . $stringToSign . PHP_EOL;
+
+        //********** begin signature ***************
+        $signatureValue = $signUtils->sha256RsaSignature($stringToSign, $private_key);
+        echo "signatureValue=" . $signatureValue . PHP_EOL;
+        echo "request path=" . $requestPath . PHP_EOL;
+        //********** end signature ***************
+
+
+        //********** begin post ***************
+
+        // Create a cURL handle
+        $ch = curl_init();
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_URL, $requestPath);  // API URL
+        curl_setopt($ch, CURLOPT_POST, true);  // POST
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonString);  // JSON Data
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'X-TIMESTAMP: ' . $timestamp,
+            'X-SIGNATURE: ' . $signatureValue,
+            'X-PARTNER-ID: ' . $merchant_id,
+        ));
+
+        // Execute the request and get the response
+        $response = curl_exec($ch);
+        echo $response . PHP_EOL;
+
+        // Check for errors
+        if ($response === false) {
+            echo 'cURL error: ' . curl_error($ch);
+        } else {
+            // Process response result
+            echo PHP_EOL;
+            echo "response=" . $response . PHP_EOL;
+        }
+
+        // Close cURL handle
+        curl_close($ch);
+        echo "=====>  end " . PHP_EOL;
+    }
+
+}
+
+$obj = new InquiryOrderStatusDemo();
+$env = "sandbox";
+$merchant_id = "sandbox-20011";
+$merchant_secret = "";
+$private_key = "";
+$trade_type = "1";
+$trade_no = "1111111";
+$order_no = "2222222";
+$obj -> doInquiryOrderStatus($env, $merchant_id, $merchant_secret, $private_key, $trade_type, $trade_no, $order_no);
+
